6d81484bfc0d5b0fa2846ca076eaefbb40dcbf34
es39 <esj10025@gmail.com>
2023-06-07 13:50:42 +0900

    :sparkles: Feat : 내비게이트 기능 추가

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Main = () => {
-  return <div>메인</div>;
+  const navigate = useNavigate();
+
+  const handleNavigateSignUp = () => {
+    navigate(`/signup`);
+  };
+
+  const handleNavigateSignIn = () => {
+    navigate(`/signin`);
+  };
+
+  return (
+    <Container>
+      <button onClick={handleNavigateSignUp}>회원가입하러 가기</button>
+      <button onClick={handleNavigateSignIn}>로그인하러 가기</button>
+    </Container>
+  );
 };
 
 export default Main;
+
+const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
