22864e82e3f57b80bc369e69b62dad2388fdc45c
es39 <esj10025@gmail.com>
2023-06-07 14:31:21 +0900

    :sparkles: Feat : 토큰 여부에 따른 리다이렉트 기능 추가

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import customAxios from "./customAxios";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const accessToken = localStorage.getItem("access_token");
 
   const handleCurrentEmail = (e) => {
     setEmail(e.target.value);
@@ -13,18 +19,43 @@ const SignIn = () => {
     setPassword(e.target.value);
   };
 
-  const handleSubmitLogin = (e) => {
+  const handleSubmitSignIn = async (e) => {
     e.preventDefault();
-    // axios 추가
+    await customAxios
+      .post(`/auth/signin`, {
+        email,
+        password,
+      })
+      .then((res) => {
+        localStorage.setItem("access_token", res.data.access_token);
+        navigate("/todo");
+      })
+      .catch((err) => console.log(err));
   };
+
+  useEffect(() => {
+    if (accessToken) {
+      navigate(`/todo`);
+    }
+  }, [accessToken]);
 
   return (
     <Container>
       <SignInBox>
-        <form>
-          <input type="text" placeholder="이메일을 입력해주세요" />
-          <input type="password" placeholder="비밀번호를 입력해주세요" />
-          <button>로그인</button>
+        <form onSubmit={handleSubmitSignIn}>
+          <input
+            type="text"
+            placeholder="이메일을 입력해주세요"
+            data-testid="email-input"
+            onChange={handleCurrentEmail}
+          />
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+            data-testid="password-input"
+            onChange={handleCurrentPassword}
+          />
+          <button data-testid="signin-button">로그인</button>
         </form>
       </SignInBox>
     </Container>
