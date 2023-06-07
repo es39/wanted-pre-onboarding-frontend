357cf1d2cae156824f6bf7e120512d3db0eb4b59
es39 <esj10025@gmail.com>
2023-06-07 18:05:00 +0900

    :lipstick: Design : 스타일 변경

--- src/Main.js
+++ src/Main.js
@@ -15,8 +15,10 @@ const Main = () => {
 
   return (
     <Container>
-      <button onClick={handleNavigateSignUp}>회원가입하러 가기</button>
-      <button onClick={handleNavigateSignIn}>로그인하러 가기</button>
+      <ButtonWrapper>
+        <ButtonStyle onClick={handleNavigateSignUp}>회원가입</ButtonStyle>
+        <ButtonStyle onClick={handleNavigateSignIn}>로그인</ButtonStyle>
+      </ButtonWrapper>
     </Container>
   );
 };
@@ -24,7 +26,30 @@ const Main = () => {
 export default Main;
 
 const Container = styled.main`
+  align-items: center;
+  flex-direction: column;
+`;
+const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+  margin: 0.5em;
+`;
+const ButtonStyle = styled.button`
+  background-color: #feb35c;
+  color: white;
+  border: none;
+  transition: all 0.2s ease 0s;
+  width: 80px;
+  height: 100%;
+  border-radius: 5px;
+  cursor: pointer;
+  text-align: center;
+  font-size: 0.85rem;
+  &:hover {
+    color: black;
+    background-color: white;
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.15);
+  }
 `;
